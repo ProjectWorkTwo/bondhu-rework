bfade1eeba06c7e7794226db39686377817ca520
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2024-01-17 13:05:14 +0600

    minor Update

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -11,17 +11,27 @@ const Home = () => {
   if (isLoadingHomePost) return <Loader />;
 
   return (
-    <section className="w-full flex justify-center items-center gap-5">
-      <div className="w-full max-w-xl py-4 flex flex-col gap-5">
-        <CreatePost />
-        <Posts>
-          {dataHomePost &&
-            dataHomePost?.map(({ _id }) => (
-              <PostHome key={_id} id={_id} refetchAll={refetchHomePost} />
-            ))}
-        </Posts>
-      </div>
-    </section>
+    <>
+      {!dataHomePost && (
+        <div className="text-center">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl pt-10">Wecolme to our Site</h1>
+          <h2>You Need to Login first to view out site</h2>
+        </div>
+      )}
+      {dataHomePost && (
+        <section className="w-full flex justify-center items-center gap-5">
+          <div className="w-full max-w-xl py-4 flex flex-col gap-5">
+            <CreatePost />
+            <Posts>
+              {dataHomePost &&
+                dataHomePost?.map(({ _id }) => (
+                  <PostHome key={_id} id={_id} refetchAll={refetchHomePost} />
+                ))}
+            </Posts>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
